7481c48768b9de1f90dcb565769df11c0828a890
PRIYANK 1 <singhumang444@gmail.com>
2023-09-16 19:20:40 +0530

    removed env node_modules

--- config/db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser :  true , useUnifiedTopology : true}).
-then(()=>{
-    console.log("Connected to MongoDB");
-}).catch((err) => console.log(err));
